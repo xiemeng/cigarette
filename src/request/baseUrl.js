03d4a920f9fc8c0aceb7ff15034762dca2a30ece
谢孟 <pactera-397@crc.com.hk>
2019-07-03 10:56:46 +0800

    晚上接口

--- src/request/baseUrl.js
+++ src/request/baseUrl.js
@@ -1,3 +1,4 @@
 export const M_BASE_URL = '/api';  //使用代理
 //export const M_BASE_URL = 'https://ecigarette.icebartech.com/api/';  //正式环境
 //export const M_BASE_URL = 'https://ecigarette.icebartech.com/api/';  //测试环境	 
+console.log(process.env.BASE_API)
